7434bc81730f1166f73601467b2b004637760146
anlitsai <altsai.tw@gmail.com>
2020-05-08 07:43:05 +0800

    update_20200508-0743

--- gasp_daily.py
+++ gasp_daily.py
@@ -17,7 +17,7 @@ import pandas as pd
 file_targetname='check_science_target_list.txt'
 cmd_targetname="cat "+file_targetname
 list_targetname=os.popen(cmd_targetname,"r").read().splitlines()
-print(list_targetname)
+#print(list_targetname)
 
 #sys.exit(0)
 
@@ -36,55 +36,54 @@ list_datetarget=[]
 # ====================================
 list_out1=[]
 list_out2=[]
+list_out3=[]
 
 file_out1='gasp_daily1.txt'
 if os.path.exists(file_out1):
     os.remove(file_out1)
+
+file_out2='gasp_daily2.txt'
+if os.path.exists(file_out2):
+    os.remove(file_out2)
     
-f_out1=open(file_out1,'w')
+file_out3='gasp_daily3.txt'
+if os.path.exists(file_out3):
+    os.remove(file_out3)
+    
+    
 
-for i in list_dailylist:
-#    print(i)
-    date=[i.split('/',2)[1]][0][3:11]
-#    print(date)
-    fitsname=[i.split('/',-1)[-1]][0]
-#    print(fitsname)
-    if "-S001" in fitsname:
-        targetname=fitsname.split('-S001',2)[0]
-    elif "@" in fitsname:
-        targetname=fitsname.split('-20',2)[0]    
-#    print(targetname)
-    date_target=date+':'+targetname
-#    print(date_target)
-    f_out1.write(date_target+'\n')
-'''
-    ###### remove duplicate ######
-    if date_target not in list_out1:
-        print(date_target)
-        list_out1.append(date_target)
-        f_out1.write(date_target+'\n')
-'''
+with open(file_out1,'w') as f_out1, open(file_out2,'w') as f_out2, open(file_out3,'w') as f_out3:
+   
+    for i in list_dailylist:
+        #print(i)
+        date=[i.split('/',2)[1]][0][3:11]
+        #print(date)
+        fitsname=[i.split('/',-1)[-1]][0]
+        #print(fitsname)
+        if "-S001" in fitsname:
+            targetname=fitsname.split('-S001',2)[0]
+        elif "@" in fitsname:
+            targetname=fitsname.split('-20',2)[0]    
+            #print(targetname)
 
+        ###### date:fitsname ######
+        date_fitsname=date+':'+fitsname
+        #print(date_fitsname)
+#        f_out1.write(date_fitsname+'\n')
+        list_out1.append(date_fitsname)
+        
+        ###### date:targetname ###### 
+        date_targetname=date+':'+targetname
+        #print(date_targetname)
+        #f_out2.write(date_fitsname+'\n')
+#        f_out2.write(date_targetname+'\n')
+        list_out2.append(date_targetname)
 
-f_out1.close
+        ###### remove duplicate ######
+        if date_targetname not in list_out3:
+#            print(date_targetname)
+            list_out3.append(date_targetname)
+            f_out3.write(date_targetname+'\n')
 
-#print(list_out1)
-
-#list_out1
-
-
-
-'''
-list_datetarget2=[]
-
-f_listout=open(file_datetarget,'w')
-
-for i in list_datetarget:
-    if i not in list_datetarget2:
-        list_datetarget2.append(i) 
-        f_listout.write(i+'\n')
-
-f_listout.close()
-
-
-print(list_datetarget2)'''
+        ###### replace targetname ######
+        
